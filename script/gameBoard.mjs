054ccf6f3f91979a156333c9e413b1b9c2d13d91
John Brown <89489797+NetRaveler@users.noreply.github.com>
2022-08-26 02:48:00 -0400

    add gameboard array and event listeners and a draw function that renders gameBoard array in DOM

--- script/gameBoard.mjs
+++ script/gameBoard.mjs
@@ -8,3 +8,24 @@ var GameboardModule = (function () {
 })();
 
 export { GameboardModule };
+
+// Example I wanted to look at later
+// const counterCreator = () => {
+//     let count = 0;
+//     return () => {
+//         console.log(count);
+//         count++;
+//     };
+// };
+
+// const counter = counterCreator();
+// const counter2 = counterCreator();
+// function fun() {
+//     console.log("running function");
+//     const twelve = "test";
+//     console.log(twelve);
+//     return 2;
+// }
+
+// const a = fun();
+// const b = fun();
--- script/main.js
+++ script/main.js
@@ -1,15 +1,46 @@
+let gameDisplay = document.getElementById("gameDisplay");
+let xMarker = "x"
+let oMarker = "o";
+let gameBoard = [xMarker, xMarker, oMarker, xMarker, xMarker, xMarker, xMarker, xMarker, xMarker,];
+
+/* Draws argument contents when passed an array */
+function draw(gameBoard) {
+    for (i = 0; i < gameBoard.length; i++) {
+
+        document.getElementById("bp" + i).innerHTML = gameBoard[i];
+
+    };
+};
+
+function mark(e) {
+    console.log("I am clicked");
+    console.log(e.target.id);
+}
+/* create methods to manipulate gameBoard Array.
+   tie event listeners to those methods
+   */
+
+/* EVENT LISTENERS */
+let bp0 = document.getElementById("bp0");
+let bp1 = document.getElementById("bp1");
+let bp2 = document.getElementById("bp2");
+let bp3 = document.getElementById("bp3");
+let bp4 = document.getElementById("bp4");
+let bp5 = document.getElementById("bp5");
+let bp6 = document.getElementById("bp6");
+let bp7 = document.getElementById("bp7");
+let bp8 = document.getElementById("bp8");
+bp0.addEventListener("click", mark);
+bp1.addEventListener("click", mark);
+bp2.addEventListener("click", mark);
+bp3.addEventListener("click", mark);
+bp4.addEventListener("click", mark);
+bp5.addEventListener("click", mark);
+bp6.addEventListener("click", mark);
+bp7.addEventListener("click", mark);
+bp8.addEventListener("click", mark);
 
 
 
-
-
-// const counterCreator = () => {
-//     let count = 0;
-//     return () => {
-//         console.log(count);
-//         count++;
-//     };
-// };
-
-// const counter = counterCreator();
-// const counter2 = counterCreator();
+/* Call functions for testing */
+draw(gameBoard);
